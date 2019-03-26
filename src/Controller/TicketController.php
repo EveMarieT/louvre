c225e945552dafcb0363e6b442c21c172a296bd4
EveMarieT <evemariethomasse@gmail.com>
2019-03-26 11:28:07 +0100

    Add entity Ticket and controller

---
+++ src/Controller/TicketController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Ticket;
+
+class TicketController extends AbstractController
+{
+    /**
+     * @Route("/ticket", name="ticket")
+     */
+    public function index()
+    {
+        return $this->render('ticket/index.html.twig', [
+            'controller_name' => 'TicketController',
+        ]);
+    }
+}
---
+++ src/Entity/Ticket.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TicketRepository")
+ */
+class Ticket
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateOfBirth;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $reducedPrice;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getDateOfBirth(): ?\DateTimeInterface
+    {
+        return $this->dateOfBirth;
+    }
+
+    public function setDateOfBirth(\DateTimeInterface $dateOfBirth): self
+    {
+        $this->dateOfBirth = $dateOfBirth;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getReducedPrice(): ?bool
+    {
+        return $this->reducedPrice;
+    }
+
+    public function setReducedPrice(bool $reducedPrice): self
+    {
+        $this->reducedPrice = $reducedPrice;
+
+        return $this;
+    }
+}
