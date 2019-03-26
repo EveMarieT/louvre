fe49f40b2a4f11cfc9495b8226942e02ea12445f
EveMarieT <evemariethomasse@gmail.com>
2019-03-26 10:02:38 +0100

    Pages creation #2 #3

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -13,6 +14,36 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/Contact", name="contact")
+     */
+    public function showContact()
+    {
+        return $this->render('pages/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/Faq", name="faq")
+     */
+    public function showFaq()
+    {
+        return $this->render('pages/faq.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/CGV", name="cgv")
+     */
+    public function showCgv()
+    {
+        return $this->render('pages/cgv.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
